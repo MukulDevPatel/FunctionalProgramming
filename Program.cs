07b9a84d0c9c1353656ab0beca312a37621fda92
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-01 16:42:18 +0530

    [Mukul Dev] Add: Functional Programming

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Functinal Programs");
+        }
+    }
+}
